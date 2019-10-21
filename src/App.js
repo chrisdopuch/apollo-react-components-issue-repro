eb0dbc66192cd1817fbadbd8df973d46fa03a2ed
Chris Dopuch <chris.dopuch@zapier.com>
2019-10-21 09:26:32 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
+// @flow
 import React from 'react';
+import { Query } from '@apollo/react-components';
+import gql from 'graphql-tag';
+
 import logo from './logo.svg';
 import './App.css';
+
+const HERO_QUERY = gql`
+  query GetCharacter($episode: String!, $offset: Int) {
+    hero(episode: $episode, offset: $offset) {
+      name
+      id
+      friends {
+        name
+        id
+        appearsIn
+      }
+    }
+  }
+`;
+
+type Hero = {
+  name: string,
+  id: string,
+  appearsIn: string[],
+  friends: Hero[],
+  ...
+};
+
+type HeroQueryVariables = {
+  episode: string,
+  offset?: ?number,
+  ...
+};
+
+class HeroQueryComp extends Query<
+  { hero: ?Hero, ... },
+  { episode: string, offset?: ?number, ... }
+> {}
 
 function App() {
   return (
@@ -18,6 +55,21 @@ function App() {
         >
           Learn React
         </a>
+        {/* Running `yarn start` will show `TypeError: Class constructor HeroQueryComp cannot be invoked without 'new'` */}
+        <HeroQueryComp query={HERO_QUERY} variables={{ episode: 'episode' }}>
+          {({ data, loading, error }) => {
+            if (loading) return 'Loading....';
+            if (error) return 'Error!';
+            if (!data || !data.hero) {
+              return <div />;
+            }
+            const hero = data.hero;
+
+            const nameAgain: string = hero.name;
+
+            return <div>{nameAgain}</div>;
+          }}
+        </HeroQueryComp>;
       </header>
     </div>
   );
